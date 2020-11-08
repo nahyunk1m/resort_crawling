6c1b10c56d030104a8335031e48054afd461eb43
nahyunk1m <kimnahyun113@gmail.com>
2020-11-08 17:39:50 +0900

    수정: 자동로그인 설정

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("Hello Wolrd!")
+import os
+import time
+import json
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+MAINPAGE = "https://www.sonohotelsresorts.com/mv.dp/dmparse.dm?menuCd=5560000"
+BACKSPACE='/ue003'
+ENTER='/ue007'
+TAB='/ue004'
+
+def get_account(info):
+    with open('secrets.json', 'r') as f:
+        data = json.loads(f.read())
+    return data[info]
+
+try:
+    driver = webdriver.Chrome()
+except:
+    pass
+
+driver.implicitly_wait(5)
+driver.get(MAINPAGE)
+id_curs = driver.find_element_by_css_selector('#_id')
+id_curs.click()
+id_curs.send_keys(get_account('ID'))
+time.sleep(2)
+password_curs = driver.find_element_by_css_selector('#_password')
+password_curs.send_keys(get_account('PW'))
+time.sleep(1)
+password_curs.send_keys(Keys.ENTER)
+time.sleep(2)
+driver.quit()
